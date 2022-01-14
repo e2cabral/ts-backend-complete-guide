22ac0a257ea266696589049dd3241f300db24165
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-14 17:26:40 -0300

    🚚 rename the hello world file to use camel case

--- src/presentation/controllers/hello-world/hello-world.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { ok } from '../../../helpers/http';
-
-export const helloWorld = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log(event);
-  return ok(JSON.stringify({ message: 'Hello World!' }));
-};
