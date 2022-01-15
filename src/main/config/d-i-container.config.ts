9adacee8dfc25d743af754875b4728a0b2a643d1
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-15 11:01:42 -0300

    ♻️ remove classes from dependency injection container

--- src/main/config/d-i-container.config.ts
+++ src/main/config/d-i-container.config.ts
@@ -1,27 +1,7 @@
 import 'reflect-metadata';
-import { Container, injectable, interfaces } from 'inversify';
+import { Container, interfaces } from 'inversify';
 import ServiceIdentifier = interfaces.ServiceIdentifier;
 import Newable = interfaces.Newable;
-
-@injectable()
-export class HelloWorld {
-  send() {
-    return 'Hello World!';
-  }
-}
-
-@injectable()
-export class Messager {
-  helloWorld: HelloWorld;
-
-  constructor(helloWorld: HelloWorld) {
-    this.helloWorld = helloWorld;
-  }
-
-  sendHelloWorldMessage() {
-    return this.helloWorld.send();
-  }
-}
 
 export class DIContainerConfig {
   static container: Container = new Container();
