b5248475c0ff6d15829ded5dd7f19ad90c9ad1a7
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-15 10:48:00 -0300

    🐛 fix the return type using an arrow function

--- src/infra/database/connection.ts
+++ src/infra/database/connection.ts
@@ -1,12 +1,12 @@
 import { createConnection } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export const connection = createConnection({
+export const Connection = () => createConnection({
   type: 'postgres',
-  host: 'localhost',
+  host: 'host.docker.internal',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'guide',
+  database: 'postgres',
   entities: [],
 } as PostgresConnectionOptions);
