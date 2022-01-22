a4518c7909ac11e63dfa3bd22d48e7c10f41da0f
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-22 11:38:25 -0300

    ✅ remove the event provided to the handler

--- tests/presentation/handlers/hello-world/hello-world.spec.ts
+++ tests/presentation/handlers/hello-world/hello-world.spec.ts
@@ -1,25 +1,9 @@
-import { APIGatewayEventRequestContextWithAuthorizer, APIGatewayProxyEvent } from 'aws-lambda';
 import { helloWorld } from '../../../../src/presentation/handlers/hello-world/helloWorld';
 import { ok } from '../../../../src/helpers/http';
 
-const mockRequest = () => ({
-  body: null,
-  headers: {},
-  multiValueHeaders: {},
-  httpMethod: 'post',
-  isBase64Encoded: false,
-  path: '/books',
-  pathParameters: null,
-  queryStringParameters: null,
-  multiValueQueryStringParameters: null,
-  stageVariables: null,
-  requestContext: {} as APIGatewayEventRequestContextWithAuthorizer<any>,
-  resource: 'create',
-} as APIGatewayProxyEvent);
-
 describe('Hello World', () => {
   test('Should return 200 on success', async () => {
-    const httpResponse = await helloWorld(mockRequest());
+    const httpResponse = await helloWorld();
     expect(httpResponse).toEqual(ok(JSON.stringify({ message: 'Hello World!', connection: 'default' })));
   });
 });
