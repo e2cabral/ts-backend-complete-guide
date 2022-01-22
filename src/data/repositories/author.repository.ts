c81fac2c0460ff87bfec743f037fdf9473e439c8
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-22 11:36:34 -0300

    ♻️ remove injectable decorator

--- src/data/repositories/author.repository.ts
+++ src/data/repositories/author.repository.ts
@@ -1,23 +1,21 @@
 import 'reflect-metadata';
 import { EntityRepository, Repository } from 'typeorm';
-import { injectable } from 'inversify';
 import { Author } from '../../domain/models/author.model';
 import { IsNullOrUndefined } from '../../helpers/error/error.helper';
 
-@injectable()
 @EntityRepository(Author)
 export class AuthorRepository extends Repository<Author> {
   async getAll(): Promise<Author[]> {
     return this.find();
   }
 
-  async getById(id: number): Promise<Author | undefined> {
+  async getById(id: number): Promise<Author> {
     try {
       const author = await this.findOne(id);
 
       IsNullOrUndefined(() => author === undefined, 'An invalid ID was provided.');
 
-      return author;
+      return author!;
     } catch (err) {
       throw Error((err as Error).message);
     }
